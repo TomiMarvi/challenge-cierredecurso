21dbf9a3538959451a90f6688c57870596a6ad1b
TomiMarvi <tomas.martinezvila@gmail.com>
2021-10-11 16:37:40 -0300

    modificando

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const mainRouter = require("./routes/mainRouter");
+
 
 
 // ************ Servidor ************
@@ -10,10 +12,7 @@ app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () => console.log("Server start in http://localhost:"+app.get("port")));
 
 
-//app.set("views", path.resolve(__dirname, "./views"));
-//app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
-app.get("/", (req,res)=>res.sendFile(path.resolve(__dirname,"./views/home.html")));
-app.get("/login", (req,res)=>res.sendFile(path.resolve(__dirname,"./views/login.html")));
-app.get("/detalle", (req,res)=>res.sendFile(path.resolve(__dirname,"./views/detalle.html")));
-app.get("/register", (req,res)=>res.sendFile(path.resolve(__dirname,"./views/register.html")));
+app.use(mainRouter);
+
